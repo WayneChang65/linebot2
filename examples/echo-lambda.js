d20de45ac0f8c037ee86ec9fcb7a0782fb79414c
satoru02 <sugiwaka02@gmail.com>
2021-09-10 15:39:07 +0900

    feature: Delete judgment of connection confirmation error

--- examples/echo-lambda.js
+++ examples/echo-lambda.js
@@ -6,28 +6,18 @@ const bot = linebot({
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 });
 
-module.exports.echo = async (event, context) => {
+module.exports.echo = async (event) => {
   if (bot.verify(event.body, event.headers["x-line-signature"])) {
     var body = JSON.parse(event.body).events[0];
     var replyToken = body.replyToken;
-    if (replyToken === '00000000000000000000000000000000') {
-      let lambdaResponse = {
-        statusCode: 200,
-        headers: {
-          "X-Line-Status": "OK"
-        },
-        body: '{ "result" : "Connection succeeded" }'
-      };
-      context.succeed(lambdaResponse);
-    } else {
-      var userText = body.message.text;
-      await bot.reply(replyToken, userText)
-        .then(function (data) {
-          console.log('Success', data);
-        }).catch(function (error) {
-          console.log('Error', error);
-        });
-    }
+    var userText = body.message.text;
+
+    await bot.reply(replyToken, userText)
+      .then(function (data) {
+        console.log('Success', data);
+      }).catch(function (error) {
+        console.log('Error', error);
+      });
   } else {
     console.log('Signature authentication error');
   }
